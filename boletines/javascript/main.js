7365ba523d69230e1a84b0d9a260ccb3d607fbf9
rommelperez <rommelperez.github.com>
2020-06-01 11:10:09 -0500

    subir boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,23 @@
+
+// obtener los elementos de la clas
+let links = document.querySelectorAll(".close")
+// rrecorrerlos
+links.forEach(function (link) {
+    // agregar un evento clic a cada uno de ellos
+    link.addEventListener('click', function (ev) {
+        ev.preventDefault();
+        let conten = document.querySelector(".conten");
+        // quitar las clases de animaion ya tienen
+        conten.classList.remove("animate__animated")
+        conten.classList.remove("animate__fadeInDown")
+        // agregar clases para su salida
+        conten.classList.add("animate__animated")
+        conten.classList.add("animate__fadeOutUp")
+
+        setTimeout(function () {
+            location.href = "/boletines";
+        }, 600);
+        return false;
+
+    });
+});
